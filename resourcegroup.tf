832d072dd8ffe83ffcafae7d5d770e909e90fa88
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 13:40:50 +0530

    god
    resource

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "~>2.46.0"
     }
   }
 }
@@ -10,10 +10,16 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id = "<azure_subscription_id>"
+  tenant_id       = "<azure_subscription_tenant_id>"
+  client_id       = "<service_principal_appid>"
+  client_secret   = "<service_principal_password>"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "test" {
+  name     = "test-resources"
+  location = "West Europe"
 }
 
 # Your code goes here
