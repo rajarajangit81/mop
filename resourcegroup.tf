ea24f3edd1e2f1da125e4bb19243ef3e6b310e98
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 14:54:49 +0530

    lst
    resource

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,24 +1,8 @@
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id       = var.SP_CLIENT_ID
-  client_secret   = var.SP_CLIENT_SECRET
-  tenant_id       = var.SP_TENANT_ID
-  version         = "=2.46.0" #Can be overide as you wish
-  features {}
-}
-
-# Create a resource group
+  subscription_id = var.subscription_id
+  # Create a resource group
 resource "azurerm_resource_group" "test" {
   name     = "test-resources"
   location = "West Europe"
+}
 }
