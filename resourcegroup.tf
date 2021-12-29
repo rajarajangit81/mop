199e30b51fccec6f1ced01aa22eff47acb78140f
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 14:56:30 +0530

    my comit
    my comit

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  subscription_id = var.subscription_id
+  subscription_id = "bfd6fa12-e23b-4cfb-8e8a-a0781abf779a"
   # Create a resource group
 resource "azurerm_resource_group" "test" {
   name     = "test-resources"
