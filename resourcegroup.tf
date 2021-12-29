5be102a4dd3a57930c4e48f5f6f38539727db1bc
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 15:46:06 +0530

    aws
    new

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,20 @@
-provider "azurerm" {
-  subscription_id = "bfd6fa12-e23b-4cfb-8e8a-a0781abf779a"
-  # Create a resource group
-resource "azurerm_resource_group" "test" {
-  name     = "test-resources"
-  location = "West Europe"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAUN56ZNTPJ4OXQMM6"
+  secret_key = "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"
+}
+
+# Create a VPC
+resource "aws_vpc" "us-east-2" {
+  cidr_block = "10.0.0.0/16"
 }
