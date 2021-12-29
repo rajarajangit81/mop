ca50c2c390e91cbadd44d935cc7fa82d2590a716
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 14:01:49 +0530

    ok
    resource

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   client_id       = var.SP_CLIENT_ID
   client_secret   = var.SP_CLIENT_SECRET
   tenant_id       = var.SP_TENANT_ID
-  version         = "=2.0.0" #Can be overide as you wish
+  version         = "=2.46.0" #Can be overide as you wish
   features {}
 }
 
