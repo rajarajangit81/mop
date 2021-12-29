acff69afdfe900f31fef228afe1c9c7afdd60aa8
rajarajangit81 <57443644+rajarajangit81@users.noreply.github.com>
2021-12-29 14:00:48 +0530

    now
    resource

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,19 +1,20 @@
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.46.0"
+      version = "=2.46.0"
     }
   }
 }
 
 provider "azurerm" {
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_CLIENT_ID
+  client_secret   = var.SP_CLIENT_SECRET
+  tenant_id       = var.SP_TENANT_ID
+  version         = "=2.0.0" #Can be overide as you wish
   features {}
-
-  subscription_id = "Free Trial"
-  tenant_id       = "4b5dd814-2758-4f61-b67b-8125caf437fc"
-  client_id       = "39ec3ceb-6677-40a1-9648-0513cedf99a8"
-  client_secret   = "f1fa9441-4df1-467c-be02-0cc81290ba51"
 }
 
 # Create a resource group
@@ -21,5 +22,3 @@ resource "azurerm_resource_group" "test" {
   name     = "test-resources"
   location = "West Europe"
 }
-
-# Your code goes here
